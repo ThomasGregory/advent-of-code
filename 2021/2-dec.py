9012a5d0e13601aee639de1c2e98450a8cf1b61c
ThomasGregory <tjg1996@live.co.uk>
2021-12-12 00:27:11 +0000

    relatively tidy approach to day 2

--- 2021/2-dec.py
+++ 2021/2-dec.py
@@ -2,24 +2,6 @@ from utils import *
 import numpy as np
 import pandas as pd
 
-# def sum_categories(dataframe, category):
-#     '''takes a 2-column pandas dataframe and sums the column 2 values of the given
-#     column 1 category'''
-#
-#     return dataframe[dataframe[0] == category][1].sum()
-
-
-# def problem1(filename, startpoint=[0,0]):
-#     '''takes in the filename of the input .txt, and outputs the solution to
-#     problem 1 as an integer'''
-#
-#     s = stringlist_to_dataframe(input_to_list(filename))
-#     horizontal_endpoint = startpoint[0] + sum_categories(s, 'forward')
-#     vertical_endpoint = startpoint[1] + sum_categories(s, 'down') - sum_categories(s, 'up')
-#
-#     return horizontal_endpoint * vertical_endpoint
-
-
 def solve_problem(filename, part=2):
     '''creates a column showing the effect of any given row on the aim. expects
     non-zero categories to be up or down'''
@@ -46,26 +28,6 @@ def solve_problem(filename, part=2):
     elif part == 2:
         return final_aimed_depth * final_horizontal
 
-
-# def problem2(filename):
-#     '''takes in the filename of the input .txt and returns the solution
-#     to problem 2. it's a big loop because we need information from
-#     the previous row, but iterrows is slow so a dataframe may no longer be
-#     the best solution'''
-#
-#     df = aim_column(stringlist_to_dataframe(input_to_list(filename)))
-#
-#     # df[df[0] = 'forward'] * df[df['aim']] =
-#
-#     print(aim_column(df))
-#
-#
-#     return 117
-
-
-
-
-
 if __name__ == "__main__":
-    print('Solution to Problem 1 is: ' + str(solve_problem('inputs/2-tutorial.txt', part = 1)))
-    print('Solution to Problem 2 is: ' + str(solve_problem('inputs/2-tutorial.txt')))
+    print('Solution to Problem 1 is: ' + str(solve_problem('inputs/2-input.txt', part = 1)))
+    print('Solution to Problem 2 is: ' + str(solve_problem('inputs/2-input.txt', part = 2)))
---
+++ 2021/utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def input_to_list(filename):
+    '''takes text file input and converts it to a list of strings, with each
+    line as its own element (created for 1-dec)'''
+
+    with open(filename) as f:
+        return [line.strip('\n') for line in f]
+
+
+def stringlist_to_dataframe(stringlist):
+    '''takes a list of strings. for each string, produces a sublist of strings
+    when this the string is split on spaces, and in turn converts this into a
+    pandas dataframe - a table, in some sense. Numerical strings are converted
+    to floats'''
+
+    df = pd.DataFrame(data=np.array([i.split() for i in stringlist]))
+
+    return df.apply(pd.to_numeric, errors = 'ignore')
+
+
+# def stringlist_to_array(stringlist):
+#     '''takes a list of strings. for each string, produces a sublist of strings
+#     when this the string is split on spaces, and in turn converts this into a
+#     numpy array - a table, in some sense. Numerical strings are converted
+#     to floats'''
+#
+#     return np.array([i.split() for i in stringlist])
