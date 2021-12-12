faf2702c296ec223e9c78ee38713fd774b51f7a8
ThomasGregory <tjg1996@live.co.uk>
2021-12-12 01:18:21 +0000

    3.1 working

--- 2021/3-dec.py
+++ 2021/3-dec.py
@@ -17,20 +17,27 @@ def column_bit(array, col=0, popularity='most'):
 def epsilon_rate(array, mode = 'epsilon'):
     '''calculates the sequence of the most frequent bits for a given array,
     and turns this into a decimal epsilon rate. Calculates gamma_rate too'''
-    print(np.shape(array)[1])
-    most_freq_bits = [str(column_bit(array, col=i)) for i in range(0, np.shape(array)[1])]
-    binary_eps = int("".join(most_freq_bits), 10)
-    print(binary_eps)
 
+    most_freq_bits = [str(column_bit(array, col=i)) for i in range(0, np.shape(array)[1])]
+    eps_rate = int("".join(most_freq_bits), 2)
 
     if mode == 'epsilon':
         return eps_rate
 
     elif mode == 'gamma':
-        return 2**len(array) - eps_rate
+        return 2**np.shape(array)[1] - eps_rate - 1
 
+def solve_problem(array, part=1):
+    '''solves problem 1: find the product of epsilon and gamma rates'''
 
+    c = character_array(input_to_list(array))
 
+    if part == 1:
+        return epsilon_rate(c) * epsilon_rate(c, mode = 'gamma')
 
-c = character_array(input_to_list('inputs/3-tutorial.txt'))
-print(epsilon_rate(c))
+    elif part == 2:
+        return 714
+
+if __name__ == "__main__":
+    print('Solution to Problem 1 is: ' + str(solve_problem('inputs/3-input.txt', part = 1)))
+    print('Solution to Problem 2 is: ' + str(solve_problem('inputs/3-tutorial.txt', part = 2)))
