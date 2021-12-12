e44716126fae4a1937b49a5d04fd577184d87161
ThomasGregory <tjg1996@live.co.uk>
2021-12-12 00:33:09 +0000

    docstring and comments up-to-date

--- 2021/2-dec.py
+++ 2021/2-dec.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 
 def solve_problem(filename, part=2):
-    '''creates a column showing the effect of any given row on the aim. expects
-    non-zero categories to be up or down'''
+    '''creates a pandas dataframe with all the necessary info at each step
+    leaves working columns explicit for readability'''
 
-    df = stringlist_to_dataframe(input_to_list(filename))
+    df = stringlist_to_dataframe(input_to_list(filename)) #turn text file to df
+
     df['aim_adjustment'] = np.zeros(len(df))
     df['horiz_adjustment'] = np.zeros(len(df))
 
@@ -19,14 +20,15 @@ def solve_problem(filename, part=2):
     df['aim'] = df['aim_adjustment'].cumsum()
     df['depth_adjustment'] = df['aim']*df['horiz_adjustment']
 
-    final_aimed_depth = df['depth_adjustment'].sum()
-    final_naive_depth = df['aim_adjustment'].sum()
+    final_aimed_depth = df['depth_adjustment'].sum() #depth for part 2
+    final_naive_depth = df['aim_adjustment'].sum() #depth for part 1
     final_horizontal = df['horiz_adjustment'].sum()
 
     if part == 1:
         return final_naive_depth * final_horizontal
     elif part == 2:
         return final_aimed_depth * final_horizontal
+
 
 if __name__ == "__main__":
     print('Solution to Problem 1 is: ' + str(solve_problem('inputs/2-input.txt', part = 1)))
