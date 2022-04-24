a005871fbb7327dd94a8252db173a706b7b0f8cf
ThomasGregory <tjg1996@live.co.uk>
2022-04-24 16:47:17 +0100

    tidyish 13-dec

--- 2021/13-dec.py
+++ 2021/13-dec.py
@@ -2,6 +2,7 @@ from utils import *
 import numpy as np
 import pandas as pd
 import time
+import matplotlib.pyplot as plt
 
 def read_input(myfile):
     '''reads the input file, and outputs:
@@ -9,21 +10,17 @@ def read_input(myfile):
         - a list of co-ordinates (hits)
         - a list of folds'''
 
-    c = input_to_list(myfile)
-    lines = len(c)
-    pivot = c.index('', lines//10) #guess that folding starts in the final 10% of the text for efficiency on long lists
-    hitstring = c[0:pivot]
-    folds = c[(pivot + 1): lines]
+    stringlist = input_to_list(myfile)
+    lines = len(stringlist)
+    pivot = stringlist.index('', lines//10) #guess that folding starts in the final 10% of the text for efficiency on long lists
+    hitstring = stringlist[0:pivot]
+    folds = stringlist[(pivot + 1): lines]
 
     xlist = [int(i.split(',')[0]) for i in hitstring]
     ylist = [int(i.split(',')[1]) for i in hitstring]
     hitlist = [xlist, ylist]
 
     size = [max(xlist), max(ylist)]
-
-    # print(size)
-    # print(hitlist)
-    # print(folds)
 
     return size, hitlist, folds
 
@@ -32,7 +29,7 @@ def create_grid(size, hitlist):
     '''generates a matrix of 1s (hits) and 0s representing the locations of the dots'''
 
     grid = np.zeros([size[1]+1, size[0]+1])
-    for coord in range(0, len(hitlist[0])): #!!for loop!!
+    for coord in range(0, len(hitlist[0])):
         grid[hitlist[1][coord], hitlist[0][coord]] = 1
 
     return grid
@@ -45,62 +42,21 @@ def do_fold(grid, fold, size):
     axis = f[0]
     line = int(f[1])
 
-    if axis == 'y':
-        altgrid = np.fliplr(grid)
-    if axis == 'x':
-        altgrid = np.flipud(grid)
-
-    print(line)
-    print(size)
-    print(grid)
-    print(axis)
     pos = 0
-
-
-    # if axis == 'y':
-    #     for j in range(line, size[1]+1):
-    #         print(j)
-    #         grid[(line - j):,:] = np.ceil((grid[j:,:] + grid[(size[1] - j):,:]) / 2)
-    #         print(grid)
-
-    # if axis == 'x':
-    #     print('\n boop \n')
-    #     for i in range(line, size[0]+1):
-    #         print(f'pos is {pos}')
-    #         grid[:,(line - pos)] = np.ceil((grid[:, line + pos] + grid[:,(line - pos)])/2)
-    #         #print(grid)
-    #         pos += 1
-    #     grid = grid[:,0:line]
-    #     #print(grid)
-    #
-    # if axis == 'y':
-    #     print('\n boop \n')
-    #     for i in range(line, size[1]+1):
-    #         print(f'pos is {pos}')
-    #         grid[(line - pos),:] = np.ceil((grid[line + pos, :] + grid[line - pos, :])/2)
-    #         #print(grid)
-    #         pos += 1
-    #     grid = grid[0:line, :]
-    #     #print(grid)
-
+    shape = np.shape(grid)
 
     if axis == 'x':
-        print('\n boop \n')
-        for pos in range(0, size[0]+1-line):
-            print(f'pos is {pos}')
+
+        for i in range(line, shape[1]):#could have refactored this better
             grid[:,(line - pos)] = np.ceil((grid[:, line + pos] + grid[:,(line - pos)])/2)
-            #print(grid)
+            pos += 1
         grid = grid[:,0:line]
-        #print(grid)
 
     if axis == 'y':
-        print('\n boop \n')
-        for i in range(0, size[1] + 1 - line):
-            print(f'pos is {pos}')
+        for i in range(line, shape[0]):
             grid[(line - pos),:] = np.ceil((grid[line + pos, :] + grid[line - pos, :])/2)
-            #print(grid)
+            pos += 1
         grid = grid[0:line, :]
-        #print(grid)
 
     return grid
 
@@ -108,28 +64,24 @@ def do_fold(grid, fold, size):
 def solve_problem():
     '''carry out the problem'''
 
-    size, hitlist, folds = read_input('inputs/13-input.txt')
+    size, hitlist, folds = read_input('inputs/13-hardmode.txt')
     mygrid = create_grid(size, hitlist)
 
     count = 0
 
     for i in folds:
+        plt.imsave(f'fold_{count}.png', mygrid)
         count += 1
         print(f'\n \n now working on fold number {count} \n \n')
         mygrid = do_fold(mygrid, i, size)
 
-
-    # mygrid = do_fold(mygrid, folds[0], size)
-    # print('\n \n warning! one fold only!\n \n')
-
-
     return mygrid, sum(sum(mygrid))
+
 
 if __name__ == "__main__":
     startTime = time.time()
-#    print('Solution to Problem 1 is: ' + read_input('inputs/13-tutorial.txt'))
-#    print('Solution to Problem 2 is: ' + str(solve_problem('inputs/5-tutorial.txt', part = 2)))
     print('testing...')
-    print(solve_problem()[0])
+    s = solve_problem()[0]
+    plt.imsave(f'hardmode.png', s)
     executionTime = (time.time() - startTime)
     print('Execution time in seconds: ' + str(executionTime))
